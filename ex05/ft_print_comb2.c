04d338d27ca9da593f0686aa33c1cb8c5de262a5
Sinethemba Mkiva <smkiva@c3r4s3.wethinkcode.co.za>
2020-06-19 16:44:02 +0200

    day02

--- ex05/ft_print_comb2.c
+++ ex05/ft_print_comb2.c
@@ -6,8 +6,39 @@
 /*   By: smkiva <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 13:59:01 by smkiva            #+#    #+#             */
-/*   Updated: 2020/06/19 14:09:36 by smkiva           ###   ########.fr       */
+/*   Updated: 2020/06/19 16:20:43 by smkiva           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include<unistd.h>
+int		ft_putchar(char c)
+{
+	write(1, &c , 1);
+}
+void	ft_print_comb2(void)
+{
+	int a;
+	int b;
 
-
+	a = -1;
+	while (a++ < 98)
+	{
+		b = a;
+		while (b++ < 99)
+		{
+			ft_putchar('0' + (a / 10));
+			ft_putchar('0' + (a % 10));
+			ft_putchar(' ');
+			ft_putchar('0' + (b / 10));
+			ft_putchar('0' + (b % 10));
+			if (a != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+		}
+	}
+}
+int main()
+{
+	ft_print_comb2();
+}
